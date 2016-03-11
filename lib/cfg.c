704b694033ef34df7b25805db8e6d93a7bf3476f
Balazs Scheidler <balazs.scheidler@balabit.com>
2016-03-11 13:55:34 +0100

    cfg: don't call host_resolve_options_init() from cfg_init
    This was actually a noop, as cfg->host_resolve_options was initialized
    based on cfg->host_resolve_options. So remove that instead.
    
    I was thinking about introducing a "init host_resolve_options
    without inheritance", but that would only be an empty function, so
    I decided against that. I could probably be convinced both ways.
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>

--- lib/cfg.c
+++ lib/cfg.c
@@ -212,7 +212,6 @@ cfg_init(GlobalConfig *cfg)
 
   dns_caching_update_options(&cfg->dns_cache_options);
   hostname_reinit(cfg->custom_domain);
-  host_resolve_options_init(&cfg->host_resolve_options, cfg);
   log_template_options_init(&cfg->template_options, cfg);
   if (!cfg_init_modules(cfg))
     return FALSE;
