97b87b629ac4453f5f0584e86d9e0ff6cc6bc9a5
Juhász Viktor <viktor.juhasz@balabit.com>
2016-05-02 10:53:05 +0200

    normalize_key: use the existing normalize_option_name instead of re-implement the buisness logic
    Signed-off-by: Juhász Viktor <viktor.juhasz@balabit.com>

--- lib/stringutils.c
+++ lib/stringutils.c
@@ -21,6 +21,7 @@
  */
 
 #include "stringutils.h"
+#include "misc.h"
 
 #include <string.h>
 
@@ -56,23 +57,8 @@ g_string_list_find_first(GList *list, const char * str, int *result_length)
   return (guchar*) user_data.start;
 }
 
-static gchar *
-str_replace_char(const gchar* str, const gchar from, const gchar to)
-{
-  gchar *p;
-  gchar *ret = g_strdup(str);
-  p = ret;
-  while (*p)
-    {
-      if (*p == from)
-        *p = to;
-      p++;
-    }
-  return ret;
-}
-
 gchar *
 normalize_key(const gchar* buffer)
 {
-  return str_replace_char(buffer, '-', '_');
+  return normalize_option_name(strdup(buffer));
 }
