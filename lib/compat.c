07e5b456d294b1dcb559a5939603be7e22474246
Juhász Viktor <viktor.juhasz@balabit.com>
2016-06-07 11:56:33 +0200

    compat_mmap: set the errno to the correct posix error value based in windows error code.
    Investigating the wine's code we collected out the possible error codes,
    set by the CreateFileMapping, MapViewOfFile and UnmapViewOfFile.
    
    These error codes can be assigned to posix error codes.
    
    Signed-off-by: Juhász Viktor <viktor.juhasz@balabit.com>

--- lib/compat.c
+++ lib/compat.c
@@ -198,42 +198,45 @@ _get_aligned_offset(off_t unaligned_offset)
   return (unaligned_offset / alignment) * alignment;
 }
 
+static int
+_map_windows_error(DWORD error)
+{
+  int result = 0;
+  switch(error)
+    {
+      case ERROR_DISK_FULL:
+        result = ENOSPC;
+        break;
+      case ERROR_ACCESS_DENIED:
+        result = EACCES;
+        break;
+      case ERROR_ALREADY_EXISTS:
+        result = EEXIST;
+        break;
+      case ERROR_UNKNOWN_REVISION:
+        result = EPERM;
+        break;
+      case ERROR_NOT_ENOUGH_MEMORY:
+        result = ENOMEM;
+        break;
+      case ERROR_SUCCESS:
+        result = 0;
+        break;
+      case ERROR_INVALID_PARAMETER:
+      case ERROR_INVALID_HANDLE:
+      case ERROR_MAPPED_ALIGNMENT:
+      case ERROR_INVALID_ADDRESS:
+      default:
+        result = EINVAL;
+        break;
+    }
+  return result;
+}
+
 void *
 mmap(void *addr, size_t len, int prot, int flags,
        int fildes, off_t off)
 {
-  /*
-   *    Win 32         		   POSIX
-   * PAGE_READONLY		PROT_READ
-   * PAGE_READWRITE		(PROT_READ | PROT_WRITE)
-   * PAGE_NOACCESS		PROT_NONE
-   * PAGE_EXECUTE		PROT_EXEC
-   * PAGE_EXECUTE_READ		(PROT_EXEC |PROT_READ)
-   * PAGE_EXECUTE_READWRITE	(PROT_EXEC | PROT_READ | PROT_WRITE)
-   *
-   *
-   * CreateFileMapping http://msdn.microsoft.com/en-us/library/aa366537(VS.85).aspx
-   *
-   * mmap and CreateFileMapping http://www.ibm.com/developerworks/systems/library/es-MigratingWin32toLinux.html#N102D8
-   *
-   * HANDLE WINAPI CreateFileMapping(
-   *   __in      HANDLE hFile,
-   *   __in_opt  LPSECURITY_ATTRIBUTES lpAttributes,
-   *   __in      DWORD flProtect,
-   *   __in      DWORD dwMaximumSizeHigh,
-   *   __in      DWORD dwMaximumSizeLow,
-   *   __in_opt  LPCTSTR lpName
-   * );
-   *
-   * LPVOID WINAPI MapViewOfFile(
-   *   __in  HANDLE hFileMappingObject,
-   *   __in  DWORD dwDesiredAccess,
-   *   __in  DWORD dwFileOffsetHigh,
-   *   __in  DWORD dwFileOffsetLow,
-   *   __in  SIZE_T dwNumberOfBytesToMap
-   *   );
-   */
-
   LARGE_INTEGER offset;
   DWORD flProtect = 0;
   DWORD dwDesiredAccess = 0;
@@ -267,32 +270,38 @@ mmap(void *addr, size_t len, int prot, int flags,
      break;
    };
 
-  file_mapping = CreateFileMapping((HANDLE)_get_osfhandle(fildes), NULL, flProtect, 0, 0, NULL);
+  HANDLE hFile = (HANDLE)_get_osfhandle(fildes);
+  if (hFile == INVALID_HANDLE_VALUE)
+    {
+      return MAP_FAILED;
+    }
+
+  file_mapping = CreateFileMapping(hFile, NULL, flProtect, 0, 0, NULL);
 
   if (file_mapping == NULL)
-    return MAP_FAILED;
+    goto error;
 
   ret_addr = MapViewOfFile(file_mapping, dwDesiredAccess, offset.HighPart, offset.LowPart, len + (off - aligned_offset));
+  if (ret_addr == NULL)
+    goto error;
 
-  /*
-   * Mapped views of a file mapping object maintain internal references to the object,
-   * and a file mapping object does not close until all references to it are released.
-   * Therefore, to fully close a file mapping object, an application must unmap all mapped
-   * views of the file mapping object by calling UnmapViewOfFile and close the file mapping
-   * object handle by calling CloseHandle. These functions can be called in any order.
-   */
+  CloseHandle(file_mapping);
+  return ret_addr;
 
-   CloseHandle(file_mapping);
-   if (ret_addr)
-     return ret_addr;
-   else
-     return MAP_FAILED;
+error:
+  errno = _map_windows_error(GetLastError());
+  return MAP_FAILED;
 }
 
 int
 munmap(void *addr, size_t len)
 {
-  return UnmapViewOfFile((void *)(_get_aligned_offset((off_t)addr))) != 0 ? 0 : -1;
+  if (!UnmapViewOfFile((void *)(_get_aligned_offset((off_t)addr))))
+    {
+      errno = _map_windows_error(GetLastError());
+      return -1;
+    }
+  return 0;
 }
 
 int
