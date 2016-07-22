f6132072b8f57af901fa57b9701e482f62bbe17c
Juhász Viktor <viktor.juhasz@balabit.com>
2016-07-22 08:12:02 +0200

    Afsql: while make distcheck, we should use the default drivers dir,
    because the prefix is overwritten by the distcheck
    
    Signed-off-by: Juhász Viktor <viktor.juhasz@balabit.com>

--- modules/afsql/afsql.c
+++ modules/afsql/afsql.c
@@ -1407,23 +1407,26 @@ afsql_dd_init(LogPipe *s)
   if (!dbi_initialized)
     {
       gint rc = dbi_initialize_r(dbi_driver_dir, &dbi_instance);
-
-      if (rc < 0)
+      if (rc <= 0)
         {
-          /* NOTE: errno might be unreliable, but that's all we have */
-          msg_error("Unable to initialize database access (DBI)",
-                    evt_tag_int("rc", rc),
-                    evt_tag_errno("error", errno),
-                    NULL);
-          goto error;
+          rc = dbi_initialize_r(NULL, &dbi_instance);
+          if (rc < 0)
+            {
+              /* NOTE: errno might be unreliable, but that's all we have */
+              msg_error("Unable to initialize database access (DBI)",
+                        evt_tag_int("rc", rc),
+                        evt_tag_errno("error", errno),
+                        NULL);
+              goto error;
+            }
+          else if (rc == 0)
+            {
+              msg_error("The database access library (DBI) reports no usable SQL drivers, perhaps DBI drivers are not installed properly",
+                        NULL);
+              goto error;
+            }
         }
-      else if (rc == 0)
-        {
-          msg_error("The database access library (DBI) reports no usable SQL drivers, perhaps DBI drivers are not installed properly",
-                    NULL);
-          goto error;
-        }
-      else
+      if (rc > 0)
         {
           dbi_initialized = TRUE;
         }
